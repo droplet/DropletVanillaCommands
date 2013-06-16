8ab96719653b9ad7360d974de6219a984b29641d
Afforess <afforess@gmail.com>
2013-06-16 18:45:15 -0400

    Rewrite for new SpoutAPI changes

--- src/main/java/org/spout/droplet/vanillacommands/DropletVanillaCommands.java
+++ src/main/java/org/spout/droplet/vanillacommands/DropletVanillaCommands.java
@@ -25,31 +25,20 @@ package org.spout.droplet.vanillacommands;
 
 import java.util.logging.Level;
 
-import org.spout.api.UnsafeMethod;
-import org.spout.api.command.CommandRegistrationsFactory;
-import org.spout.api.command.annotated.AnnotatedCommandRegistrationFactory;
-import org.spout.api.command.annotated.SimpleAnnotatedCommandExecutorFactory;
-import org.spout.api.command.annotated.SimpleInjector;
-import org.spout.api.plugin.CommonPlugin;
+import org.spout.api.command.annotated.AnnotatedCommandExecutorFactory;
+import org.spout.api.plugin.Plugin;
 
-import org.spout.droplet.vanillacommands.commands.VanillaCommand;
+import org.spout.droplet.vanillacommands.commands.VanillaCommands;
 
-public class DropletVanillaCommands extends CommonPlugin {
+public class DropletVanillaCommands extends Plugin {
 	@Override
-	@UnsafeMethod
 	public void onDisable() {
 		getLogger().log(Level.INFO, getDescription().getVersion() + " disabled");
 	}
 
 	@Override
-	@UnsafeMethod
 	public void onEnable() {
 		getLogger().log(Level.INFO, getDescription().getVersion() + " enabled");
-
-		/**
-		 * Commands
-		 */
-		CommandRegistrationsFactory<Class<?>> commandRegFactory = new AnnotatedCommandRegistrationFactory(new SimpleInjector(this), new SimpleAnnotatedCommandExecutorFactory());
-		getEngine().getRootCommand().addSubCommands(this, VanillaCommand.class, commandRegFactory);
+		AnnotatedCommandExecutorFactory.create(new VanillaCommands());
 	}
 }
--- src/main/java/org/spout/droplet/vanillacommands/commands/VanillaCommands.java
+++ src/main/java/org/spout/droplet/vanillacommands/commands/VanillaCommands.java
@@ -23,66 +23,244 @@
  */
 package org.spout.droplet.vanillacommands.commands;
 
-import org.spout.api.command.CommandContext;
+import org.spout.api.command.CommandArguments;
 import org.spout.api.command.CommandSource;
 import org.spout.api.command.annotated.Command;
-import org.spout.api.command.annotated.CommandPermissions;
+import org.spout.api.command.annotated.Permissible;
+import org.spout.api.entity.Player;
+import org.spout.api.exception.CommandException;
+import org.spout.api.geo.discrete.Point;
 
-import org.spout.droplet.vanillacommands.DropletVanillaCommands;
+import org.spout.vanilla.component.entity.misc.Burn;
+import org.spout.vanilla.component.entity.misc.Effects;
+import org.spout.vanilla.component.entity.misc.Health;
+import org.spout.vanilla.component.entity.misc.Hunger;
+import org.spout.vanilla.data.effect.EntityEffect;
+import org.spout.vanilla.data.effect.EntityEffectType;
+import org.spout.vanilla.data.effect.ExplosionEffect;
+import org.spout.vanilla.event.cause.HealthChangeCause;
 
 public class VanillaCommands {
-	DropletVanillaCommands plugin;
-	CommandMethods CM = new CommandMethods();
-
-	public VanillaCommands(DropletVanillaCommands instance) {
-		plugin = instance;
-	}
-
+	/**
+	 * Used for starving the player.
+	 * @throws CommandException 
+	 */
 	@Command(aliases = {"starve"}, usage = "<player> <hunger level>", desc = "Sets the hunger level of the player.")
-	@CommandPermissions("vanillacommands.starve")
-	public void starve(CommandContext args, CommandSource source) {
-		CM.starve(args, source);
+	@Permissible("vanillacommands.starve")
+	public void starve(CommandSource source, CommandArguments args) throws CommandException {
+		if (args.length() == 0) {
+			if (source instanceof Player) {
+				((Player)source).get(Hunger.class).setHunger(0);
+				source.sendMessage("Succesfully set your hunger to zero");
+			} else {
+				source.sendMessage("You are not a player!");
+			}
+		} else {
+			if (args.isPlayer(0)) {
+				int hungerLevel = 0;
+				if (args.length() == 2 && args.isInteger(1)) {
+					hungerLevel = args.getInteger(1);
+				}
+				args.getPlayer(0).get(Hunger.class).setHunger(hungerLevel);
+				source.sendMessage("Succesfully set " + args.getString(0) + "'s hunger to " + hungerLevel);
+			} else {
+				source.sendMessage(args.getString(0) + " is not a player!");
+			}
+		}
 	}
 
+	/**
+	 * Used for setting the health of the player.
+	 * @throws CommandException 
+	 */
 	@Command(aliases = {"hurt", "damage"}, usage = "<player> <health level>", desc = "Sets the health level of the player.")
-	@CommandPermissions("vanillacommands.hurt")
-	public void hurt(CommandContext args, CommandSource source) {
-		CM.hurt(args, source);
+	@Permissible("vanillacommands.hurt")
+	public void hurt(CommandSource source, CommandArguments args) throws CommandException {
+		if (args.length() == 0) {
+			if (source instanceof Player) {
+				((Player)source).get(Health.class).setHealth(1, HealthChangeCause.COMMAND);
+				source.sendMessage("Succesfully set your health to one");
+			} else {
+				source.sendMessage("You are not a player!");
+			}
+		} else {
+			if (args.isPlayer(0)) {
+				int healthLevel = 0;
+				if (args.length() == 2 && args.isInteger(1)) {
+					healthLevel = args.getInteger(1);
+				}
+				args.getPlayer(0).get(Health.class).setHealth(healthLevel, HealthChangeCause.COMMAND);
+				source.sendMessage("Succesfully set " + args.getString(0) + "'s health to " + healthLevel);
+			} else {
+				source.sendMessage(args.getString(0) + " is not a player!");
+			}
+		}
 	}
 
+	/**
+	 * Used for setting the player on fire
+	 * @throws CommandException 
+	 */
 	@Command(aliases = {"ignite"}, usage = "<player>", desc = "Sets the player on fire.")
-	@CommandPermissions("vanillacommands.ignite")
-	public void ignite(CommandContext args, CommandSource source) {
-		CM.ignite(args, source);
+	@Permissible("vanillacommands.ignite")
+	public void ignite(CommandSource source, CommandArguments args) throws CommandException {
+		if (args.length() == 0) {
+			if (source instanceof Player) {
+				((Player)source).get(Burn.class).setOnFire(20, true);
+				source.sendMessage("Succesfully set you on fire!");
+			} else {
+				source.sendMessage("You are not a player!");
+			}
+		} else {
+			if (args.isPlayer(0)) {
+				args.getPlayer(0).get(Burn.class).setOnFire(20, true);
+				source.sendMessage("Succesfully set " + args.getString(0) + " on fire!");
+			} else {
+				source.sendMessage(args.getString(0) + " is not a player!");
+			}
+		}
 	}
 
+	/**
+	 * Used for teleporting the player high up
+	 * @throws CommandException 
+	 */
 	@Command(aliases = {"fall"}, usage = "<player>", desc = "Makes the player fall.")
-	@CommandPermissions("vanillacommands.fall")
-	public void fall(CommandContext args, CommandSource source) {
-		CM.fall(args, source);
+	@Permissible("vanillacommands.fall")
+	public void fall(CommandSource source, CommandArguments args) throws CommandException {
+		if (args.length() == 0) {
+			if (source instanceof Player) {
+				Player player = (Player)source;
+				Point point = player.getScene().getPosition();
+				Point newPoint = new Point(point.getWorld(), point.getBlockX(), (point.getBlockY() + 60), point.getBlockZ());
+				player.teleport(newPoint);
+				source.sendMessage("Succesfully fell!");
+			} else {
+				source.sendMessage("You must be a player!");
+				source.sendMessage("Or use /c fall <player>");
+			}
+		} else {
+			if (args.isPlayer(0)) {
+				int distance = 60;
+				if (args.length() == 2 && args.isInteger(1)) {
+					distance = args.getInteger(1);
+				}
+				Player player = args.getPlayer(0);
+				Point point = player.getScene().getPosition();
+				Point newPoint = new Point(point.getWorld(), point.getBlockX(), (point.getBlockY() + distance), point.getBlockZ());
+				source.sendMessage("Succesfully made " + args.getString(0) + " fall " + distance + " blocks!");
+				player.teleport(newPoint);
+			} else {
+				source.sendMessage(args.getString(0) + " is not a player!");
+			}
+		}
 	}
 
+	/**
+	 * Used for creating an explosion around the player
+	 * @throws CommandException 
+	 */
 	@Command(aliases = {"explode"}, usage = "<player>", desc = "Makes the player explode.")
-	@CommandPermissions("vanillacommands.explode")
-	public void explode(CommandContext args, CommandSource source) {
-		CM.explode(args, source);
+	@Permissible("vanillacommands.explode")
+	public void explode(CommandSource source, CommandArguments args) throws CommandException {
+		if (args.length() == 0) {
+			if (source instanceof Player) {
+				Player player = ((Player)source);
+				Point point = player.getScene().getPosition();
+				ExplosionEffect explosion = new ExplosionEffect(10, 4);
+				explosion.play(player, point);
+				player.get(Health.class).setHealth(0, HealthChangeCause.COMMAND);
+				source.sendMessage("Succesfully exploded!");
+			} else {
+				source.sendMessage("You are not a player!");
+				source.sendMessage("Or use /c explode <player>");
+			}
+		} else {
+			if (args.isPlayer(0)) {
+				Player player = args.getPlayer(0);
+				Point point = player.getScene().getPosition();
+				ExplosionEffect explosion = new ExplosionEffect(10, 4);
+				explosion.play(player, point);
+				player.get(Health.class).setHealth(0, HealthChangeCause.COMMAND);
+				source.sendMessage("Succesfully exploded!");
+			} else {
+				source.sendMessage(args.getString(0) + " is not a player!");
+			}
+		}
 	}
 
+	/**
+	 * Used for blinding a player
+	 * @throws CommandException 
+	 */
 	@Command(aliases = {"blind"}, usage = "<player>", desc = "Makes the player blind.")
-	@CommandPermissions("vanillacommands.blind")
-	public void blind(CommandContext args, CommandSource source) {
-		CM.blind(args, source);
+	@Permissible("vanillacommands.blind")
+	public void blind(CommandSource source, CommandArguments args) throws CommandException {
+		Player player = null;
+		if (args.length() == 0) {
+			player = (Player)source;
+		} else if (args.length() == 1) {
+			if (args.isPlayer(0)) {
+				player = args.getPlayer(0);
+			} else {
+				source.sendMessage(args.getString(0) + " is not a player!");
+			}
+		}
+		if (player != null) {
+			player.add(Effects.class).add(new EntityEffect(EntityEffectType.BLINDNESS, 3F));
+			source.sendMessage("Succesfully made " + player.getName() + " blind!");
+		} else {
+			source.sendMessage("Syntax: /c blind <player>");
+		}
 	}
 
+	/**
+	 * Used for giving the player the Nausea effect
+	 * @throws CommandException 
+	 */
 	@Command(aliases = {"ill"}, usage = "<player>", desc = "Makes the player ill.")
-	@CommandPermissions("vanillacommands.ill")
-	public void ill(CommandContext args, CommandSource source) {
-		CM.ill(args, source);
+	@Permissible("vanillacommands.ill")
+	public void ill(CommandSource source, CommandArguments args) throws CommandException {
+		Player player = null;
+		if (args.length() == 0) {
+			player = (Player)source;
+		} else if (args.length() == 1) {
+			if (args.isPlayer(0)) {
+				player = args.getPlayer(0);
+			} else {
+				source.sendMessage(args.getString(0) + " is not a player!");
+			}
+		}
+		if (player != null) {
+			player.add(Effects.class).add(new EntityEffect(EntityEffectType.NAUSEA, 3F));
+			source.sendMessage("Succesfully made " + player.getName() + " ill!");
+		} else {
+			source.sendMessage("Syntax: /c ill <player>");
+		}
 	}
 
+	/**
+	 * Used for giving the player the hunger effect
+	 * @throws CommandException 
+	 */
 	@Command(aliases = {"hungry"}, usage = "<player>", desc = "Makes the player hungry.")
-	@CommandPermissions("vanillacommands.hungry")
-	public void hungry(CommandContext args, CommandSource source) {
-		CM.hungry(args, source);
+	@Permissible("vanillacommands.hungry")
+	public void hungry(CommandSource source, CommandArguments args) throws CommandException {
+		Player player = null;
+		if (args.length() == 0) {
+			player = (Player)source;
+		} else if (args.length() == 1) {
+			if (args.isPlayer(0)) {
+				player = args.getPlayer(0);
+			} else {
+				source.sendMessage(args.getString(0) + " is not a player!");
+			}
+		}
+		if (player != null) {
+			player.add(Effects.class).add(new EntityEffect(EntityEffectType.HUNGER, 3F));
+			source.sendMessage("Succesfully made " + player.getName() + " hungry!");
+		} else {
+			source.sendMessage("Syntax: /c hungry <player>");
+		}
 	}
 }
